af43f67c45e227cfea1a61616183fdf2726ce2f2
Богдан <sysoevbogdan82@gmail.com>
2024-08-31 15:08:23 +0300

    set up the project

---
+++ tailwind.config.ts
@@ -0,0 +1,34 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: 'var(--font-inter)',
+      },
+      colors: {
+        bgPrimary: 'var(--bg-primary)',
+        textPrimary: 'var(--text-primary)',
+        textDark: 'var(--text-dark)',
+        textHover: 'var(--text-hover)',
+      },
+      screens: {
+        xs: '380px',
+        sm: '420px',
+        md: '768px',
+        ml: '1024px',
+        lg: '1280px',
+        xl: '1440px',
+        '2xl': '1920px',
+      },
+    },
+  },
+  plugins: [require('tailwind-scrollbar')],
+};
+
+export default config;
